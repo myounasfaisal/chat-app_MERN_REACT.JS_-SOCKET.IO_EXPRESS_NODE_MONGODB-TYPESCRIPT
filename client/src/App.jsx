9c8e01635cf32ca2488b9cc9bc48d8ace06327a9
myounasfaisal <younasf69@gmail.com>
2025-04-27 15:03:25 +0500

    Integrated Daisy UI

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col'>
+        
+<button className="btn btn-soft btn-primary">Primary</button>
+<button className="btn btn-soft btn-secondary">Secondary</button>
+<button className="btn btn-soft btn-accent">Accent</button>
+<button className="btn btn-soft btn-info">Info</button>
+<button className="btn btn-soft btn-success">Success</button>
+<button className="btn btn-soft btn-warning">Warning</button>
+<button className="btn btn-soft btn-error">Error</button>
+      </div>
+    </>
+  )
+}
+
+export default App
